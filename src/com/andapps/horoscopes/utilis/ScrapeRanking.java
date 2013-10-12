c84c1804253e990ee945085bdf4b1e88d2a326bb
rmarji <raed@jogeeks.com>
2013-10-12 17:02:50 +0300

    started working on v0.9
    this will contain another Utils class that handles scraping the play
    storeand submitting to the api

--- src/com/andapps/horoscopes/utilis/ScrapeRanking.java
+++ src/com/andapps/horoscopes/utilis/ScrapeRanking.java
@@ -1,5 +1,14 @@
 package com.andapps.horoscopes.utilis;
 
+import android.content.Context;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
 public class ScrapeRanking {
 
+	public static void logRanking(Context c){
+	TelephonyManager tm = (TelephonyManager)c.getSystemService(Context.TELEPHONY_SERVICE);
+	String countryCode = tm.getSimCountryIso();
+	Log.d("country", countryCode);
+	}
 }
